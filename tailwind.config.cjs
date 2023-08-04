fa886ed4cc6b0bd88b0d5884242852ee60eeb9c2
Jakub Serwatka <kubaserw@gmail.com>
2023-08-04 16:59:24 +0200

    feat: add footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,13 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         colors: {
-            "text-primary": "#D31D1D",
-            "text-secondary": "#3A2E2E",
-            "text-tertiary": "#FFF3F2",
-            "background-primary": "#FFF3F2",
-            "background-lilac": "#E4DDFF",
-            "background-ice-blue": "#E0F4FF",
-            "background-tea-green": "#D1E9D4",
-            "background-volt": "#EFF32C"
+            "ruby-red": "#D31D1D",
+            "dark-chocolate": "#3A2E2E",
+            "light-pink": "#FFF3F2",
+            lilac: "#E4DDFF",
+            "ice-blue": "#E0F4FF",
+            "tea-green": "#D1E9D4",
+            volt: "#EFF32C"
         },
         fontFamily: {
             metropolis: ["Metropolis", "Arial", "sans-serif"],
