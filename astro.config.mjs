6a2556837b0d7379aa763337f2aa0b4ba0df6104
Jakub Serwatka <kubaserw@gmail.com>
2023-08-04 11:39:20 +0200

    chore: configure base font classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind({
+            applyBaseStyles: false
+        })
+    ],
     experimental: {
         assets: true
     }
