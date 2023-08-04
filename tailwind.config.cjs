8d7d12c069269c203b315ab64fe2484e79c248c9
Jakub Serwatka <kubaserw@gmail.com>
2023-08-04 08:56:28 +0200

    chore: configure theme colors tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        colors: {
+            "text-primary": "#D31D1D",
+            "text-secondary": "#3A2E2E",
+            "text-tertiary": "#FFF3F2",
+            "background-primary": "#FFF3F2",
+            "background-lilac": "#E4DDFF",
+            "background-ice-blue": "#E0F4FF",
+            "background-tea-green": "#D1E9D4",
+            "background-volt": "#EFF32C"
+        },
+        extend: {}
+    },
+    plugins: []
+};
