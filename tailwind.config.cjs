13ee5583c7c6edb1c29385b17de1893999f6efb4
Jakub Serwatka <kubaserw@gmail.com>
2023-08-12 13:27:38 +0200

    chore: extend tailwidn colors instead of overriding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,6 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        colors: {
-            "ruby-red": "#D31D1D",
-            "dark-chocolate": "#3A2E2E",
-            "light-pink": "#FFF3F2",
-            lilac: "#E4DDFF",
-            "ice-blue": "#E0F4FF",
-            "tea-green": "#D1E9D4",
-            volt: "#EFF32C"
-        },
         fontFamily: {
             metropolis: ["Metropolis", "Arial", "sans-serif"],
             olympic: ["OlympicLight", "Palatino", "serif"],
@@ -37,6 +28,15 @@ module.exports = {
                 "header_4-max": "1.75rem",
                 "paragraph_normal-max": "1.75rem",
                 "paragraph_quote-max": "1.75rem"
+            },
+            colors: {
+                "ruby-red": "#D31D1D",
+                "dark-chocolate": "#3A2E2E",
+                "light-pink": "#FFF3F2",
+                lilac: "#E4DDFF",
+                "ice-blue": "#E0F4FF",
+                "tea-green": "#D1E9D4",
+                volt: "#EFF32C"
             }
         }
     },
