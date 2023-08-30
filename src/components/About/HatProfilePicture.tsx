1f75bfa50f8f0e3ef2aadb6e94e482a28af8ad15
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2023-08-30 20:15:31 +0200

    Feature/main button (#9)
    * feat: create btn primary css class
    
    * feat: unify buttons and their behaviour

--- src/components/About/HatProfilePicture.tsx
+++ src/components/About/HatProfilePicture.tsx
@@ -112,13 +112,21 @@ const HatProfilePicture = () => {
                 </picture>
                 <button
                     type="button"
-                    class="absolute bottom-[7%] left-[10%] right-0 z-10 mx-auto max-w-fit rounded-3xl border-2 border-ruby-red bg-light-pink px-6 py-1 text-center text-xl"
+                    class="btn-primary absolute bottom-[7%] left-[10%] right-0 z-10 mx-auto max-w-fit bg-light-pink"
                     onClick={changeHat}
                 >
-                    Next hat <span class="font-extralight">&#10140; </span>
+                    Next hat
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="17"
+                        viewBox="0 0 16 17"
+                        class="ml-4 inline-block -rotate-90 fill-ruby-red"
+                    >
+                        <path d="M7.29289 16.7071C7.68342 17.0976 8.31658 17.0976 8.70711 16.7071L15.0711 10.3431C15.4616 9.95262 15.4616 9.31946 15.0711 8.92893C14.6805 8.53841 14.0474 8.53841 13.6569 8.92893L8 14.5858L2.34315 8.92893C1.95262 8.53841 1.31946 8.53841 0.928932 8.92893C0.538407 9.31946 0.538407 9.95262 0.928932 10.3431L7.29289 16.7071ZM7 -4.37114e-08L7 16L9 16L9 4.37114e-08L7 -4.37114e-08Z"></path>
+                    </svg>
                 </button>
             </div>
-
             <picture>
                 <source srcset={`${hats[currentIndex()].src}.avif`} type="image/avif" />
                 <img
