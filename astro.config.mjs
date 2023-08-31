07ab8b2631abdb03514834d03cae02711318631a
Jakub Serwatka <kubaserw@gmail.com>
2023-08-31 16:32:51 +0200

    chore: update astro to 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), solidJs()],
-  experimental: {
-    assets: true
-  }
+    integrations: [
+        tailwind({
+            applyBaseStyles: false
+        }),
+        solidJs()
+    ]
 });
