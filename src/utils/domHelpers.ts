f65ec19ff0484d7952110abeac6b7a4c03b6fade
Jakub Serwatka <kubaserw@gmail.com>
2023-09-29 18:58:56 +0200

    feat: finished - use inView method

--- src/utils/domHelpers.ts
+++ src/utils/domHelpers.ts
@@ -1,10 +1,19 @@
-export function getElementAbsolutePosition(element: HTMLElement): { top: number; left: number } {
+interface AbsoluteDomRect {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+}
+
+export function getAbsoluteBoundingClientReact(element: HTMLElement): AbsoluteDomRect {
     const rect = element.getBoundingClientRect();
     const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
     return {
         top: rect.top + scrollTop,
-        left: rect.left + scrollLeft
+        left: rect.left + scrollLeft,
+        width: rect.width,
+        height: rect.height
     };
 }
