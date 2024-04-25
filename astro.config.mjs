ed0777013dbd6f2f681dad5c60de3a9a7ba0f1ef
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2024-04-25 17:42:40 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            applyBaseStyles: false
-        }),
-        solidJs()
-    ]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), solidJs(), sitemap()]
 });
