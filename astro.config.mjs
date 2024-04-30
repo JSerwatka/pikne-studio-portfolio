750015a23e10f5452c84fb5fa271f31e85d9e679
Jakub Serwatka <jserwatka.dev@gmail.com>
2024-04-30 22:38:28 +0200

    feat: add sitemap (#73)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), solidJs(), sitemap()]
+    site: "https://pikne.studio",
+    integrations: [
+        tailwind({
+            applyBaseStyles: false
+        }),
+        solidJs(),
+        sitemap()
+    ]
 });
